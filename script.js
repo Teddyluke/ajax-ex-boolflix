d5a3421363cda5060c889a4b00062076de6c0a3a
Luca Maglie <luca.maglie2@gmail.com>
2020-07-28 16:10:25 +0200

    milestone 2 incompleta. Attesa correzione di domani.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-// https://api.themoviedb.org/3/movie/550?api_key=e887da77e61cb551591cb5cd06107a79 MY API
-
-
 // Milestone 1:Creare un layout base con una searchbar (una input e un button) in cui possiamoscrivere completamente o parzialmente il nome di un film. Possiamo, cliccando ilbottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ognifilm trovato:1.Titolo2.Titolo Originale3.Lingua4.Voto
 
 
@@ -24,7 +20,6 @@ function searchMovies() {
     },
     success: function (data) {
       var results = data["results"];
-      // template title name
       var template = $("#searched-movie-template").html();
       var compiled = Handlebars.compile(template);
       var target = $(".titles");
@@ -59,7 +54,8 @@ function searchSeries(searchVal) {
       for (var i = 0; i < results.length; i++) {
         var targetHTML = compiled(results[i])
         target.append(targetHTML);
-        // var vote = results[i]['vote_average'];
+        var vote = results[i]['vote_average'];
+        getStarsRating(vote);
     }
     },
     error: function (error) {
@@ -76,11 +72,12 @@ function getStarsRating(vote){
   var compiled = Handlebars.compile(template);
   var target = $(".titles");
   var star = " ";
-  for (var i = 1; i <= 5; i++) {
-    if (i < vote) {
-      star += '<i class="fa fa-star"> </i>';
+  for (var i = 0; i < 5; i++) {
+    console.log(starsRating);
+    if (i < starsRating) {
+      star += '<i class="fas fa-star"> </i>';
     } else {
-      star += '<i class="fa fa-star"> </i>';
+      star += '<i class="far fa-star"> </i>';
     }
   }
   var targetHTML = compiled({
@@ -88,10 +85,15 @@ function getStarsRating(vote){
   });
   target.append(targetHTML);
 }
-
+function testing() {
+  var test1 = "it";
+  var test2 = 'https://www.countryflags.io/'+ test1 +'/shiny/32.png'
+  console.log(test2);
+}
 
 function init() {
 searchBtnListener();
+testing();
 }
 
 $(document).ready(init);
