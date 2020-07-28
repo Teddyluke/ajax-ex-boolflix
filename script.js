e1324804f9efdedd38dce295e61c81f1208dcfc1
Luca Maglie <luca.maglie2@gmail.com>
2020-07-28 11:35:03 +0200

    tentativi casuali di risoluzione dell'esercizio

--- script.js
+++ script.js
@@ -13,28 +13,51 @@ function searchBtnListener() {
 function searchRequest() {
   var searchVal = $("#search").val();
   $(".titles").text("");
+  $("#search").val("");
   $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=' + searchVal + '&language=it',
+    url: 'https://api.themoviedb.org/3/search/movie',
     method: "GET",
+    data: {
+      'api_key':'e99307154c6dfb0b4750f6603256716d',
+      'query' : searchVal,
+      'language' : 'it'
+    },
     success: function (data) {
       var results = data["results"];
       var template = $("#searched-movie-template").html();
       var compiled = Handlebars.compile(template);
       var target = $(".titles");
       for (var i = 0; i < results.length; i++) {
-        var targetHTML = compiled({
-        "it-title": results[i].title,
-        "original_title": results[i].original_title,
-        "original_language": results[i].original_language,
-        "vote_average": results[i].vote_average
-      })
-      target.append(targetHTML);
+        var targetHTML = compiled(results[i])
+        target.append(targetHTML);
+        var vote = results[i]['vote_average'];
+        getStarsRating(vote)
     }
     },
     error: function (error) {
       console.log(error);
     }
   })
+}
+
+function getStarsRating(vote){
+  var starsRating = Math.ceil(vote / 2);
+  var template = $("#star-rating").html();
+  var compiled = Handlebars.compile(template);
+  var target = $(".titles");
+  var targetHTML;
+  for (var i = 0; i < 5; i++) {
+    if(i<vote){
+      targetHTML = compiled({
+        "stellapiena": "<i class='fa fa-star'></i>",
+      });
+    }else{
+      targetHTML = compiled({
+        "stellavuota": "<i class='far fa-star'></i>"
+      });
+    }
+    target.append(targetHTML);
+  }
 }
 
 
